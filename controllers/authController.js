cb2622db24c487139ebf29792128e9dbf3f89e3d
L7O7L <luisbaltazarsegura@gmail.com>
2023-04-01 17:36:39 -0500

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,106 @@
+const jwt = require('jsonwebtoken')
+const bcryptjs = require('bcryptjs')
+const conexion = require('../database/db')
+const {promisify} = require('util')
+
+//procedimiento para registrarnos
+exports.register = async (req, res)=>{    
+    try {
+        const nombre = req.body.nombre
+        const apellidos = req.body.apellidos
+        const email = req.body.email
+        const pass = req.body.pass
+        let passHash = await bcryptjs.hash(pass, 8)    
+        //console.log(passHash)   
+        conexion.query('INSERT INTO usuarios SET ?', {nombre:nombre, apellidos: apellidos, email: email, password: passHash}, (error, results)=>{
+            if(error){console.log(error)}
+            res.redirect('/')
+        })
+    } catch (error) {
+        console.log(error)
+    }       
+}
+
+exports.login = async (req, res)=>{
+    try {
+        const email = req.body.email
+        const pass = req.body.pass        
+
+        if(!email || !pass ){
+            res.render('login',{
+                alert:true,
+                alertTitle: "Advertencia",
+                alertMessage: "Ingrese un usuario y password",
+                alertIcon:'info',
+                showConfirmButton: true,
+                timer: false,
+                ruta: 'login'
+            })
+        }else{
+
+            conexion.query('SELECT * FROM usuarios WHERE email = ?', [email], async (error, results)=>{
+
+                if( results.length == 0 || ! (await bcryptjs.compare(pass, results[0].password)) ){
+                    res.render('login', {
+                        alert: true,
+                        alertTitle: "Error",
+                        alertMessage: "Usuario y/o Password incorrectas",
+                        alertIcon:'error',
+                        showConfirmButton: true,
+                        timer: false,
+                        ruta: 'login'    
+                    })
+                }else{
+                    //inicio de sesión OK
+                    const id = results[0].id
+                    const token = jwt.sign({id:id}, process.env.JWT_SECRETO, {
+                        expiresIn: process.env.JWT_TIEMPO_EXPIRA
+                    })
+                    //generamos el token SIN fecha de expiracion
+                   //const token = jwt.sign({id: id}, process.env.JWT_SECRETO)
+                   console.log("TOKEN: "+token+" para el USUARIO : "+email)
+
+                   const cookiesOptions = {
+                        expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+                        httpOnly: true
+                   }
+                   res.cookie('jwt', token, cookiesOptions)
+                   res.render('login', {
+                        alert: true,
+                        alertTitle: "Conexión exitosa",
+                        alertMessage: "¡LOGIN CORRECTO!",
+                        alertIcon:'success',
+                        showConfirmButton: false,
+                        timer: 800,
+                        ruta: ''
+                   })
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.isAuthenticated = async (req, res, next)=>{
+    if (req.cookies.jwt) {
+        try {
+            const decodificada = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRETO)
+            conexion.query('SELECT * FROM usuarios WHERE id = ?', [decodificada.id], (error, results)=>{
+                if(!results){return next()}
+                req.email = results[0]
+                return next()
+            })
+        } catch (error) {
+            console.log(error)
+            return next()
+        }
+    }else{
+        res.redirect('/login')        
+    }
+}
+
+exports.logout = (req, res)=>{
+    res.clearCookie('jwt')   
+    return res.redirect('/')
+}
